d0434f9392dd6cf9d8447b8fe93f00d6fa55f024
homuncus <cot.cotenov@gmail.com>
2022-10-05 08:51:13 +0300

    updated PostController

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,8 +1,13 @@
 'use strict'
 
+const { RuntimeException } = require('@adonisjs/lucid/src/Exceptions')
+
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Post = use('App/Models/Post')
+const Helpers = use('Helpers')
 
 /**
  * Resourceful controller for interacting with posts
@@ -10,7 +15,7 @@
 class PostController {
   /**
    * Show a list of all posts.
-   * GET posts
+   * GET /
    *
    * @param {object} ctx
    * @param {Request} ctx.request
@@ -18,6 +23,8 @@ class PostController {
    * @param {View} ctx.view
    */
   async index ({ request, response, view }) {
+    const posts = await Post.all()
+    view.render('index', {posts: posts})
   }
 
   /**
@@ -30,17 +37,42 @@ class PostController {
    * @param {View} ctx.view
    */
   async create ({ request, response, view }) {
+    view.render('addpost');
   }
 
   /**
    * Create/save a new post.
-   * POST posts
+   * POST posts/create
    *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store ({ request, response, session }) {
+    const {theme, text} = request.all()
+    const img = request.file('img', {
+      types: ['image'],
+      size: '2mb',
+      extnames: ['png', 'jpg', 'jpeg']
+    })
+
+    try{
+      await img.move(Helpers.tmpPath('uploads'), {
+        name: `${new Date().getTime()}.${file.subtype}`
+      })
+      if (!img.moved()) {
+        throw new RuntimeException
+      }
+      const post = new Post()
+      post.theme = theme
+      post.text = text
+      post.img_path = img.fileName
+      await post.save()
+      session.flash({success: 'Sucessfully created a post'})
+    } catch(e) {
+      session.flash({error: 'Error creating a post'})
+    }
+    response.redirect('back');
   }
 
   /**
@@ -53,6 +85,9 @@ class PostController {
    * @param {View} ctx.view
    */
   async show ({ params, request, response, view }) {
+    const {id} = params
+    const post = await Post.find(id)
+    return view.render('post-show', {post: post})
   }
 
   /**
@@ -65,6 +100,9 @@ class PostController {
    * @param {View} ctx.view
    */
   async edit ({ params, request, response, view }) {
+    const {id} = params
+    const post = await Post.find(id)
+    return view.render('post-edit', {post: post})
   }
 
   /**
@@ -75,7 +113,30 @@ class PostController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params, request, response, session }) {
+    const {id} = params
+    const {theme, text} = request.all()
+    const img = request.file('img', {
+      types: ['image'],
+      size: '2mb',
+      extnames: ['png', 'jpg', 'jpeg']
+    })
+    const post = await Post.find(id)
+    try{
+      if(theme) post.theme = theme
+      if(text) post.text = text
+      if(img) {
+        await img.move(Helpers.tmpPath('uploads'), {
+          name: `${new Date().getTime()}.${file.subtype}`
+        })
+        if (!img.moved()) {
+          throw new RuntimeException
+        }
+        post.img_path = img.fileName
+      }
+    } catch(e) {
+      session.flash({error: 'Error updating the post'})
+    }
   }
 
   /**
@@ -87,6 +148,10 @@ class PostController {
    * @param {Response} ctx.response
    */
   async destroy ({ params, request, response }) {
+    const {id} = params;
+    const post = await Post.find(id)
+    await post.delete()
+    response.redirect('back')
   }
 }
 
